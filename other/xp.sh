b338da0ec5a1d2e97115e30830d823d6836bf7cb
Dugong <110967975+dugong-lewat@users.noreply.github.com>
2023-12-21 22:10:16 +0700

    Add files via upload

--- other/xp.sh
+++ other/xp.sh
@@ -47,17 +47,54 @@ rm -rf /user/log-trojan-$user.txt
 fi
 done
 
+# SS
+data=($(cat /usr/local/etc/xray/config.json | grep '^#!' | cut -d ' ' -f 2 | sort | uniq))
+now=$(date +"%Y-%m-%d")
+for user in "${data[@]}"; do
+exp=$(grep -w "^#! $user" "/usr/local/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(((d1 - d2) / 86400))
+if [[ "$exp2" -le "0" ]]; then
+sed -i "/^#! $user $exp/,/^},{/d" /usr/local/etc/xray/config.json
+sed -i "/^#! $user $exp/,/^},{/d" /usr/local/etc/xray/config.json
+rm -rf /var/www/html/ss/ss-$user.txt
+rm -rf /user/log-ss-$user.txt
+fi
+done
+
+# SS2022
+data=($(cat /usr/local/etc/xray/config.json | grep '^#%' | cut -d ' ' -f 2 | sort | uniq))
+now=$(date +"%Y-%m-%d")
+for user in "${data[@]}"; do
+exp=$(grep -w "^#% $user" "/usr/local/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(((d1 - d2) / 86400))
+if [[ "$exp2" -le "0" ]]; then
+sed -i "/^#% $user $exp/,/^},{/d" /usr/local/etc/xray/config.json
+sed -i "/^#% $user $exp/,/^},{/d" /usr/local/etc/xray/config.json
+rm -rf /var/www/html/ss2022/ss2022-$user.txt
+rm -rf /user/log-ss2022-$user.txt
+fi
+done
+
 # ALLXRAY
 data=($(cat /usr/local/etc/xray/config.json | grep '^#&@' | cut -d ' ' -f 2 | sort | uniq))
 now=$(date +"%Y-%m-%d")
 for user in "${data[@]}"; do
 exp=$(grep -w "^#&@ $user" "/usr/local/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+exp1=$(grep -w "^#&@ $user+" "/usr/local/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
+d12=$(date -d "$exp" +%s)
+d22=$(date -d "$now" +%s)
 exp2=$(((d1 - d2) / 86400))
+exp3=$(((d12 - d22) / 86400))
 if [[ "$exp2" -le "0" ]]; then
+if [[ "$exp3" -le "0" ]]; then
 sed -i "/^#&@ $user $exp/,/^},{/d" /usr/local/etc/xray/config.json
-sed -i "/^#&@ $user $exp/,/^},{/d" /usr/local/etc/xray/config.json
+sed -i "/^#&@ $user+ $exp3/,/^},{/d" /usr/local/etc/xray/config.json
 rm -rf /var/www/html/allxray/allxray-$user.txt
 rm -rf /user/log-allxray-$user.txt
 fi
