b93899fec6917f779e1309c26883689272eee9c3
Dugong <110967975+dugong-lewat@users.noreply.github.com>
2023-12-21 11:40:25 +0700

    Fix error UUID

--- trojan/add-trojan.sh
+++ trojan/add-trojan.sh
@@ -30,7 +30,7 @@ uuid=$(cat /proc/sys/kernel/random/uuid)
 read -p "Expired (days): " masaaktif
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 sed -i '/#universal$/a\#& '"$user $exp"'\
-},{"password": "'""$uuid""'","email": "'""$user""'"' /usr/local/etc/xray/config.json
+},{"id": "'""$uuid""'","email": "'""$user""'"' /usr/local/etc/xray/config.json
 sed -i '/#trojan$/a\#& '"$user $exp"'\
 },{"password": "'""$uuid""'","email": "'""$user""'"' /usr/local/etc/xray/config.json
 sed -i '/#trojan-tcp$/a\#& '"$user $exp"'\
