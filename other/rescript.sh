154165b7b82d29bd925a060ea0f9130edac32be8
Dugong <110967975+dugong-lewat@users.noreply.github.com>
2024-01-19 18:30:12 +0700

    Update rescript.sh

--- other/rescript.sh
+++ other/rescript.sh
@@ -1,15 +1,4 @@
-NC='\e[0m'
-DEFBOLD='\e[39;1m'
-RB='\e[31;1m'
-GB='\e[32;1m'
-YB='\e[33;1m'
-BB='\e[34;1m'
-MB='\e[35;1m'
-CB='\e[35;1m'
-WB='\e[37;1m'
-
-echo -e "${YB}[ WARNING ] All users will be deleted (Y/N)${NC} "
-read answer
+echo -e "[ WARNING ] All users will be deleted (Y/N)${NC}" read answer
 if [ "$answer" == "${answer#[Yy]}" ] ;then
 exit 0
 else
