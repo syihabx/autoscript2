386351b01a482ad060a94e01f84ca6b5ef964f1a
Syihab <122454815+syihabx@users.noreply.github.com>
2024-04-20 17:54:44 +0700

    Update certxray.sh

--- other/certxray.sh
+++ other/certxray.sh
@@ -12,6 +12,14 @@ echo -e "${GB}[ INFO ]${NC} ${YB}Start${NC} "
 sleep 0.5
 systemctl stop nginx
 domain=$(cat /var/lib/dnsvps.conf | cut -d'=' -f2)
+CF_GlobalKey=""
+CF_AccountEmail=""
+echo -e "Please set the API key:"
+read -p "Input your key here:" CF_GlobalKey
+echo -e "Your API key is:${CF_GlobalKey}"
+echo -e "Please set up registered email:"
+read -p "Input your email here:" CF_AccountEmail
+echo -e "Your registered email address is:${CF_AccountEmail}"
 Cek=$(lsof -i:80 | cut -d' ' -f1 | awk 'NR==2 {print $1}')
 if [[ ! -z "$Cek" ]]; then
 sleep 1
@@ -23,9 +31,16 @@ sleep 1
 fi
 echo -e "${GB}[ INFO ]${NC} ${YB}Starting renew cert...${NC} "
 sleep 2
+~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+if [ $? -ne 0 ]; then
+    echo -e "Default CA, Lets'Encrypt fail, script exiting..."
+    exit 1
+fi
+export CF_Key="${CF_GlobalKey}"
+export CF_Email=${CF_AccountEmail}
 cd .acme.sh
-bash acme.sh --issue -d $domain --dns dns_cf --server letsencrypt --keylength ec-256 --fullchain-file /usr/local/etc/xray/fullchain.crt --key-file /usr/local/etc/xray/private.key --force \
---dns dns_cf --dns dns_cf_email syihabx@gmail.com --dns dns_cf_key 35151d347cf9ce456d71c5137d550b2138c1f
+cd .acme.sh
+bash acme.sh --issue --dns dns_cf -d $domain --keylength ec-256 --fullchain-file /usr/local/etc/xray/fullchain.crt --key-file /usr/local/etc/xray/private.key --standalone --force
 echo -e "${GB}[ INFO ]${NC} ${YB}Renew cert done...${NC} "
 sleep 2
 echo -e "${GB}[ INFO ]${NC} ${YB}Starting service $Cek${NC} "
